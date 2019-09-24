5ffa766555df2a88632cfd8810f16634fc95153e
Rok Mandeljc <rok.mandeljc@gmail.com>
2019-09-24 22:50:16 +0200

    playlists.py: python3 lacks cmp() function and cmp keyword in sort()

--- spydaap/playlists.py
+++ spydaap/playlists.py
@@ -14,8 +14,6 @@
 # along with Spydaap. If not, see <http://www.gnu.org/licenses/>.
 
 
-from past.builtins import cmp
-
 import os
 import time
 
@@ -29,7 +27,9 @@ class Playlist(object):
 
     def safe_cmp(self, a, b, key):
         if key in a and key in b:
-            return cmp(a[key], b[key])
+            def compat_cmp(x, y):
+                return (x > y) - (x < y)
+            return compat_cmp(a[key], b[key])
         elif key in a:
             return 1
         elif key in b:
@@ -97,7 +97,27 @@ class YearRange(Playlist):
                                                'daap.songartist',
                                                'daap.songalbum',
                                                'daap.songtracknumber'])
-        entries.sort(cmp=s)
+
+        def cmp_to_key(mycmp):
+            'Convert a cmp= function into a key= function'
+            class K(object):
+                def __init__(self, obj, *args):
+                    self.obj = obj
+                def __lt__(self, other):
+                    return mycmp(self.obj, other.obj) < 0
+                def __gt__(self, other):
+                    return mycmp(self.obj, other.obj) > 0
+                def __eq__(self, other):
+                    return mycmp(self.obj, other.obj) == 0
+                def __le__(self, other):
+                    return mycmp(self.obj, other.obj) <= 0
+                def __ge__(self, other):
+                    return mycmp(self.obj, other.obj) >= 0
+                def __ne__(self, other):
+                    return mycmp(self.obj, other.obj) != 0
+            return K
+
+        entries.sort(key=cmp_to_key(s))
 
 
 class Recent(Playlist):
